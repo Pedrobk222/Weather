8c726f105e3f8645b75e4ead6cc8efe27bb10646
Pedro Henrique Bortolucci  Klayn <74112579+Pedrobk222@users.noreply.github.com>
2024-06-28 16:18:22 -0300

    Update WeatherConditionTranslator.js

--- weather-app/src/components/WeatherConditionTranslator.js
+++ weather-app/src/components/WeatherConditionTranslator.js
@@ -4,6 +4,7 @@ const weatherConditions = {
   Sunny: 'Ensolarado',
   Clear: 'Limpo',
   'Partly Cloudy ': 'Parcialmente nublado',
+  'Partly cloudy': 'Parcialmente nublado',
   Cloudy: 'Nublado',
   Overcast: 'Encoberto',
   'Patchy rain nearby': 'Chuva irregular nas proximidades',
